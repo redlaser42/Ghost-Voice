379844c4e1d37fd3b68ba306e336941dbadec889
redlaser42 <120990408+redlaser42@users.noreply.github.com>
2026-06-22 00:59:13 -0400

    Init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Jelly Voices
+---
+This is a mod that adds various voices to SPT. Over time as i find good clips I'll flush out the voices more. Do let me know if there are any problems also the voices are a bit loud. I get all my voice lines from recordings of the games their from or archived streams.
+
+## Showcase
+[![Video](https://youtu.be/wys8Pn2Zxr0)](https://youtu.be/wys8Pn2Zxr0)
+
+## Voice Request
+You can leave a suggestion for a voice to add in the comments.
+Requirements:
+- Name
+- What there from
+- A video link to voice lines, with little background noise (optional)
+
+## Voice List
+- Jerma985
+- Hunk (RE9 & RE:Operation Raccoon City)
+- Leon (RE4 remake)
+- Ada Wong (RE4 remake)
+- Stalker Bandit (Stalker)
+- Talon (COD MW2019 & Warzone)
+- More to come!
+
+## Installing
+Just extract the folder and drop it in the main SPT directory. Make sure to extract using winrar or 7zip.
+
+## Uninstalling
+Before uninstalling make sure you change your voice back to a default. Then you can safely delete the "**JellyVoices**" folder in your "**SPT/user/mods**".
+
+## Modding Guide
+I made a guide on how to make voice mods on my Github page in the wiki section.
---
+++ VisualStudioProject/main.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using Range = SemanticVersioning.Range;
+
+namespace GhostVoice;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.redlaser42.ghostvoice";
+    public override string Name { get; init; } = "Ghost Voice";
+    public override string Author { get; init; } = "redlaser42";
+    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+    public override Range SptVersion { get; init; } = new("~4.0.1");
+    public override string License { get; init; } = "MIT";
+    public override bool? IsBundleMod { get; init; } = true;
+    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    {
+        { "com.wtt.commonlib", new Range("~2.0.0") }
+    };
+    public override string? Url { get; init; }
+    public override List<string>? Contributors { get; init; }
+    public override List<string>? Incompatibilities { get; init; }
+}
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 2)]
+public class GhostVoice(
+    WTTServerCommonLib.WTTServerCommonLib wttCommon
+) : IOnLoad
+{
+    public async Task OnLoad()
+    {
+
+        // Get your current assembly
+        var assembly = Assembly.GetExecutingAssembly();
+
+
+        // Use WTT-CommonLib services
+        await wttCommon.CustomVoiceService.CreateCustomVoices(assembly);
+
+        await Task.CompletedTask;
+    }
+}
